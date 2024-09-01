41286604db08a7e7fa9d8605578ac04187471a35
Fernando Salomao <fsmesquita@outlook.com>
2024-09-01 19:20:09 -0300

    Created BigO Class

---
+++ BigO/BigOLessons.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FirstRepo.BigO;
+
+public class BigO
+{
+    public void FindWord(string word, string[] items)
+    {
+        Console.WriteLine($"Finding word \"{word}\" in a array of {items.Length} items");
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstRepo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var bigO = new BigO.BigO();
+
+        bigO.FindWord("fernando", args);
+
+        Console.WriteLine($"Program executed");
+    }
+}
